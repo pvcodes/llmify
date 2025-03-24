67f4d817c83df25583b27a5d3ea84f18bdd7fc55
Pranjal Verma <pvcodes@gmail.com>
2025-03-24 19:20:21 +0530

    pricing fix

--- src/components/landing-page/HeroCards.tsx
+++ src/components/landing-page/HeroCards.tsx
@@ -133,7 +133,7 @@ export const HeroCards = () => {
             </Badge>
           </CardTitle>
           <div>
-            <span className="text-3xl font-bold">$0</span>
+            <span className="text-3xl font-bold">₹0</span>
             <span className="text-muted-foreground"> /month</span>
           </div>
 
