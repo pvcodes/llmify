18bd7381b33595d0c6eb653920cc7ceeaa8210e2
Pranjal Verma <pvcodes@Pranjals-MacBook-Air.local>
2025-05-13 15:08:37 +0530

    status route

--- src/app/api/status/route.ts
+++ src/app/api/status/route.ts
@@ -1,9 +1,17 @@
 import { NextResponse } from 'next/server';
 
+export async function GET() {
+	try {
+		return Response.json('ok');
+	} catch (error) {
+		return NextResponse.json((error as Error).message, { status: 400 });
+	}
+}
+
 export async function POST() {
-  try {
-    Response.json('ok');
-  } catch (error) {
-    return NextResponse.json((error as Error).message, { status: 400 });
-  }
+	try {
+		return Response.json('ok');
+	} catch (error) {
+		return NextResponse.json((error as Error).message, { status: 400 });
+	}
 }
